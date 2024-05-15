c800fd4d1cd896747e48905119f5404cd9914347
Skule Webmaster <webmaster@skule.ca>
2024-05-15 02:40:23 -0400

    fix message close button

--- index.js
+++ index.js
@@ -83,11 +83,3 @@ if (isSignInWithEmailLink(auth, window.location.href)) {
         });
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-    (document.querySelectorAll('.notification .delete') || []).forEach(($delete) => {
-        const $notification = $delete.parentNode;
-        $delete.addEventListener('click', () => {
-            $notification.parentNode.removeChild($notification);
-        });
-    });
-});
